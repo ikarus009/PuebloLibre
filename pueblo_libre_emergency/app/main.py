3ba65d4d3b5b5a26b5a646c18fd5ed522561223c
Ismael Sacha <39897987+ikarus009@users.noreply.github.com>
2024-08-07 09:44:04 -0500

    main.py

--- pueblo_libre_emergency/app/main.py
+++ pueblo_libre_emergency/app/main.py
@@ -1,14 +1,8 @@
 import streamlit as st
-from pages import serenazgo_platform, citizen_app, sereno_app
 
-st.set_page_config(page_title="Sistema de Emergencia Pueblo Libre", layout="wide")
+def main():
+    st.title("Mi Aplicación Streamlit")
+    st.write("Hola, Streamlit!")
 
-st.sidebar.title("Navegación")
-page = st.sidebar.radio("Ir a", ["Plataforma de Serenazgo", "App del Ciudadano", "App del Sereno"])
-
-if page == "Plataforma de Serenazgo":
-    serenazgo_platform.show()
-elif page == "App del Ciudadano":
-    citizen_app.show()
-elif page == "App del Sereno":
-    sereno_app.show()
+if __name__ == "__main__":
+    main()
